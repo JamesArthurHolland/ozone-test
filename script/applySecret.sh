6f3de46d53c0cd9baad45741e6922d724061312e
Jamie Holland <jholland@dnsfilter.com>
2021-07-28 17:51:06 -0400

    Pipeline update.

--- script/applySecret.sh
+++ script/applySecret.sh
@@ -3,8 +3,8 @@
 if [ -f "$SECRET_FILE" ]; then
   echo "Adding secret file $SECRET_FILE to namespace $NAMESPACE"
   echo $kubectl apply -f $SECRET_FILE -n "$NAMESPACE"
-elif [[ -v "$K8S_SECRET_BASE64" ]]; then
-  echo "$K8S_SECRET_BASE64" | base64 -d | kubectl apply -f -
+elif [[ -v "$SECRET_BASE64" ]]; then
+  echo "$SECRET_BASE64" | base64 -d | kubectl apply -f -
 else
     printf "\n\n ERROR: Please add the file: \n\n $SECRET_FILE or env var K8S_SECRET_BASE64 \n\n"
     exit 1
