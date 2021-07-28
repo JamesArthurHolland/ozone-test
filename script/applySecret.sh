ac89701c4c9dc2ec91315d809c20c79a1caba137
Jamie Holland <jholland@dnsfilter.com>
2021-07-28 19:59:37 -0400

    Ozone file update.

--- script/applySecret.sh
+++ script/applySecret.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+env
+
 if [ -f "$SECRET_FILE" ]; then
   echo "Adding secret file $SECRET_FILE to namespace $NAMESPACE"
   echo $kubectl apply -f $SECRET_FILE -n "$NAMESPACE"
 elif [ -n "$SECRET_BASE64" ]; then
   echo "$SECRET_BASE64" | base64 -d | kubectl apply -f -
 else
-    printf "\n\n ERROR: Please add the file: \n\n $SECRET_FILE or env var SECRET_BASE64 \n\n"
+    printf "\n\n ERROR: Please add the filepath var: \n\n SECRET_FILE or env var SECRET_BASE64 \n\n"
     exit 1
 fi
