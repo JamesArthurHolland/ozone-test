553b9419c929fa9d7aa6624fdcaf15e38a8a462c
Jamie Holland <jholland@dnsfilter.com>
2021-08-02 22:50:13 -0400

    Debug - Apply secret for named branch.

--- script/applySecret.sh
+++ script/applySecret.sh
@@ -4,7 +4,7 @@ env
 
 if [ -f "$SECRET_FILE" ]; then
   echo "Adding secret file $SECRET_FILE to namespace $NAMESPACE"
-  echo $kubectl apply -f $SECRET_FILE -n "$NAMESPACE"
+  kubectl apply -f $SECRET_FILE -n "$NAMESPACE"
 elif [ -n "$SECRET_BASE64" ]; then
   echo "$SECRET_BASE64" | base64 -d | kubectl apply -n "$NAMESPACE" -f -
 else
